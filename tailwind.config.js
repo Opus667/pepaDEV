3abd8250f8b8de4bc8bd577d9c644ec6f9a22107
Pepa <pepa@pepa.dev.br>
2025-11-20 04:36:15 -0300

    Começando a ficar ´serio

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./**/*.html", "!./node_modules"],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {},
+    },
+  },
+  variants: {},
+  plugins: [require("@tailwindcss/typography")],
+};
